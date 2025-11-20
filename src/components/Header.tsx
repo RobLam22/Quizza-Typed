1d395a361cf5b0747eb0718fb66cf7df84231a66
Robert Lam <robert.lam22@gmail.com>
2025-11-20 18:13:38 +1100

    Navigation-menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext.jsx';
 import { useState } from 'react';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
 
 export default function Header() {
     const { session, signOutUser } = useAuth();
@@ -22,6 +32,42 @@ export default function Header() {
     return (
         <header className="bg-white">
             <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+                <NavigationMenu className="md:hidden">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="size-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>
+                                    <Link to="/">Home</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink>
+                                    <Link to="/about">About</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink>
+                                    <Link to="/leaderboard">Leaderboard</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink>
+                                    <Link to="/dashboard">Dashboard</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
                 <div className="flex flex-1 items-center justify-end md:justify-between">
                     <nav aria-label="Global" className="hidden md:block">
                         <ul className="flex items-center gap-6 text-sm">
@@ -90,24 +136,6 @@ export default function Header() {
                                 </Link>
                             )}
                         </div>
-
-                        <button className="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                            <span className="sr-only">Toggle menu</span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="size-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        </button>
                     </div>
                 </div>
             </div>
